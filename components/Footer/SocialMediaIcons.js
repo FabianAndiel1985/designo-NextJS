2e4e85df41f378b12469e78ee3a3e14ce1a96089
Fabian Andiel <fabian.andiel@gmail.com>
2024-10-04 17:04:42 +0200

    further work on footer, adds social media

--- components/Footer/SocialMediaIcons.js
+++ components/Footer/SocialMediaIcons.js
@@ -1,13 +1,24 @@
+'use client'
+
 import React from 'react'
 import "./SocialMediaIcons.scss"
+import Image from 'next/image'
 
 const SocialMediaIcons = () => {
 
-  //"/logo-dark.png"
+ const icons = ["facebook","youtube","twitter","pinterest","instagram"];
 
   return (
     <div id='social-media-icons'>
- 
+      { icons.map((e)=>(
+        <Image
+            src={"/icon-" +e+".svg"}
+            width={24}
+            height={24}
+        />
+    )
+    )
+    }
     </div>
   )
 }
