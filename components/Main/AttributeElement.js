cb8ae01ba4fbc0d51de17d9fcf3509bf84c37efc
Fabian Andiel <fabian.andiel@gmail.com>
2024-10-18 13:30:31 +0200

    changes attribute elements

--- components/Main/AttributeElement.js
+++ components/Main/AttributeElement.js
@@ -5,14 +5,16 @@ import "./AttributeElement.scss";
 
 const AttributeElement = ({element}) => {
   return (
-    <div className={"Attribute_Element"}>
+    <div className={"attribute-element"}>
         <Image
         src={element.imgSrc}
         width={202}
         height={202}
         />
-        <h1> {element.heading.toUpperCase()} </h1>
-        <p> {element.text}</p>
+        <div className={"attribute-element__texts"}>
+            <h1> {element.heading.toUpperCase()} </h1>
+            <p> {element.text}</p>
+        </div>
     </div>
   )
 }
