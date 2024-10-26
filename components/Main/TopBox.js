e34451d185b22a5c005458ed50e1f5a652c8efef
Fabian Andiel <fabian.andiel@gmail.com>
2024-10-26 17:29:03 +0200

    changes

--- components/Main/TopBox.js
+++ components/Main/TopBox.js
@@ -7,17 +7,9 @@ import Image from 'next/image';
 export const TopBox = () => {
   return (
     <div className={"top-box"}>
-        <div className={"top-box__text-box"}>
-            <h1>Award-winning custom designs and digital branding solutions</h1>
-            <p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
-            <Button text={"learn more"} />
-        </div>
+        <div>
 
-        <div className={"top-box__image-box"}>
-            <Image 
-            src={"/image-hero-phone.png"}
-            fill={true}
-            />
+
         </div>
     </div>
   )
