5cad5a1775e7adbe83828b18004cbe0748fb21f1
Fabian Andiel <fabian.andiel@gmail.com>
2024-10-26 18:17:32 +0200

    finishes top box

--- components/Main/TopBox.js
+++ components/Main/TopBox.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import "./TopBox.scss";
 import Button from './Button';
-import Image from 'next/image';
-
 
 export const TopBox = () => {
   return (
     <div className={"top-box"}>
         <div>
-
-
+        <h1>Award-winning custom designs and digital branding solutions</h1>
+        <p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+        <Button text={"learn more"} />        
         </div>
     </div>
   )
