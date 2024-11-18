47cb8c8c491cefef5365e0c48e905c62449ce42d
Fabian Andiel <fabian.andiel@gmail.com>
2024-11-18 10:34:31 +0100

    adds illustrations for locations and data

---
+++ mocks/LocationData.js
@@ -0,0 +1,19 @@
+import Button from "@/components/Main/Button";
+
+export const locationElementContent=[
+    {
+    imgSrc: "/illustration-canada.svg",  
+    heading:"canada",
+    text:(<Button text={"See location"}/>)
+    },
+    {
+      imgSrc: "/illustration-australia.svg",  
+      heading:"australia",
+      text:(<Button text={"See location"}/>)
+    },
+    {
+      imgSrc: "/illustration-uk.svg",  
+      heading:"united kingdom",
+      text:(<Button text={"See location"}/>)
+    }
+];
