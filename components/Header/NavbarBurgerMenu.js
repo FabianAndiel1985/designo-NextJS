0af46d14ae25ddf41ec1831cbb1748f72254d84b
Fabian Andiel <fabian.andiel@gmail.com>
2024-12-03 10:28:50 +0100

    improves burger menu

--- components/Header/NavbarBurgerMenu.js
+++ components/Header/NavbarBurgerMenu.js
@@ -1,37 +1,33 @@
 'use client';
 import React from 'react'
 import Image from 'next/image';
-import { useContext, useState} from "react";
+import { useContext} from "react";
 import { AppContext } from "@/context/context";
 
 
 
 const NavbarBurgerMenu = ({isHeader}) => {
 
-
-  const [showBurgerIcon, setShowBurgerIcon] = useState(true);
-  const {toggleBurgerMenu} = useContext(AppContext);
-
-
+  const {toggleBurgerMenu, showBurgerMenu} = useContext(AppContext);
 
   return (
    <>
-   { showBurgerIcon  && isHeader &&
+   { !showBurgerMenu  && isHeader &&
           <Image
             src={"/icon-hamburger.svg"}
             width={16}
             height={16}
             className='navbar__hamburger-icon'
-            onClick={()=>{ setShowBurgerIcon(false);toggleBurgerMenu(true)}}
+            onClick={()=>{toggleBurgerMenu(true)}}
           />
           }
           {
-            !showBurgerIcon && isHeader &&
+            showBurgerMenu && isHeader &&
           <Image
             src={"/icon-close.svg"}
             width={20}
             height={20}
-            onClick={()=>{  setShowBurgerIcon(true);toggleBurgerMenu(false) }}        
+            onClick={()=>{toggleBurgerMenu(false) }}        
             className='navbar__close-icon'
           />
         }
