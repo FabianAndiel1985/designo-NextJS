70037cfa10c477e822b2c92a8ef1a8ce4d263cc2
Fabian Andiel <fabian.andiel@gmail.com>
2024-12-03 11:08:15 +0100

    smaller refactorings

--- components/Header/NavbarTexts.js
+++ components/Header/NavbarTexts.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import "./NavbarTexts.scss";
 
 const NavbarTexts = ({isHeader, brightLinks, hideOnMobile}) => {
-    const navbarLinks = [{title:"our copany", link:"/"}, {title:"locations", link:"/"}, {title:"contact", link:"/"} ];
+    const navbarLinks = [{title:"our copany", link:"/"}, {title:"locations", link:"/"}, {title:"contact", link:"/contact"} ];
     return (
         <>
         <div className={`navbar__links ${isHeader ? '' : 'navbar__links__footer'} ${brightLinks ? 'brightLinks' : 'darkLinks'} ${hideOnMobile && 'hideLinks'} ` }>
